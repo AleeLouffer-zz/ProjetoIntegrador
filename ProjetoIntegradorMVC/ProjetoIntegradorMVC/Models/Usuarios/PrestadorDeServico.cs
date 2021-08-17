c487f9e36c6f2672e5abac67b138de4935fd012e
Zamecki <mateuszamecki@hotmail.com>
2021-08-17 15:30:40 -0300

    v2

---
+++ ProjetoIntegradorMVC/ProjetoIntegradorMVC/Models/Usuarios/PrestadorDeServico.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetoIntegradorMVC.Models.Usuarios
+{
+    public class PrestadorDeServico : IUsuario
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+
+        public List<string> Servicos { get; set; }
+        public List<Decimal> ContasAReceber { get; set; }
+    }
+}
