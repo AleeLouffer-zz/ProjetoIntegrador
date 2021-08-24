ac55ab0f9dce27a575cb3efa1cd210ddd3203a25
Jean <jeanvbonimenezes@gmail.com>
2021-08-24 13:57:47 -0400

    Adicionando os repositorios e introduzindo o conceito de base

---
+++ ProjetoIntegradorMVC/ProjetoIntegradorMVC/Repositorio/Interface/IRepositorioCliente.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjetoIntegradorMVC.Models.Operacoes;
+using ProjetoIntegradorMVC.Models.Usuarios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetoIntegradorMVC.Models.Repositorio.Interface
+{
+    public interface IRepositorioCliente : IRepositorio<Cliente>
+    {
+
+    }
+}
---
+++ ProjetoIntegradorMVC/ProjetoIntegradorMVC/Repositorio/RepositorioPrestador.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjetoIntegradorMVC.Models.ContextoDb;
+using ProjetoIntegradorMVC.Models.Repositorio.Interface;
+using ProjetoIntegradorMVC.Models.Usuarios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetoIntegradorMVC.Models.Repositorio
+{
+    public class RepositorioPrestador : RepositorioBase<PrestadorDeServico> , IRepositorioPrestador
+    {
+        public RepositorioPrestador(bool SaveChanges = true) : base(SaveChanges)
+        {
+
+        }
+    }
+}
